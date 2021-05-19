309bec71bc20193d901e835a43ce3efd579f3643
Davide <d.sadotti@gmail.com>
2021-05-19 12:58:27 +0200

    Integrazione del framework vue.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,37 +64,35 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+        <div class="content" id="app">
+            <h2>tutti i film</h2>
+            
+            <ul>
+                <li v-for="film in films">
+                    <h3>@{{ film.title }}</h3>
+                </li>
+            </ul>
         </div>
+
+        {{-- AXIOS CDN --}}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js" integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        {{-- VUE CDN --}}
+        <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
+
+        <script>
+            var app = new Vue({
+                el: '#app',
+                data: {
+                    films: []
+                },
+                mounted: function(){
+                    axios.get('/api/movies')
+                    .then((response) => {
+                        this.films = response.data;
+                    })
+                }
+            })
+        </script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('movies.index');
+    // return redirect()->route('movies.index');
+    return view('welcome');
 });
 
 Route::resource('movies', 'MovieController');
