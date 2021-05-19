9b0d7b3694a26c0c13756f82e0a07e03fcf65b6d
Davide <d.sadotti@gmail.com>
2021-05-19 10:05:53 +0200

    Creazione tabelle DB

---
+++ database/migrations/2021_05_19_080022_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('film_director', 50);
+            $table->string('genres', 50);
+            $table->text('plot');
+            $table->string('year', 4);
+            $table->string('cover_image')->default('https://via.placeholder.com/600');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
